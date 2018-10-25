14367ec82ac62a8c80a7af07f8c59272cdb4c1e8
repkit <ionut.baches@yahoo.com>
2018-10-25 09:45:21 +0000

    improvement on get client ip address

--- src/RpkUtils/Sysinfo/Client.php
+++ src/RpkUtils/Sysinfo/Client.php
@@ -29,10 +29,13 @@
           // to check ip is pass from proxy
           $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
           
-        } else {
+        } elseif (!empty($_SERVER['REMOTE_ADDR'])) {
             
           $ip = $_SERVER['REMOTE_ADDR'];
           
+        } else {
+            
+            $ip = 0.0.0.0;
         }
         
         return $ip;
