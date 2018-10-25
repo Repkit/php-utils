14a7c3f6c6c5119bc04f6ab832f3a801ae2f7336
repkit <ionut.baches@yahoo.com>
2018-10-25 09:10:06 +0000

    update get client ip address

--- src/RpkUtils/Sysinfo/Client.php
+++ src/RpkUtils/Sysinfo/Client.php
@@ -20,15 +20,20 @@
      */
     public static function ip()
     {
-        if(!empty($_SERVER['REMOTE_ADDR'])){
-            $ip = $_SERVER['REMOTE_ADDR'];
-        }else{
-            /*dunno secure alternative as tring to get from other $_SERVER values
-            generate security problems 
-            @link: http://roshanbh.com.np/2007/12/getting-real-ip-address-in-php.html*/
-            $ip = '0.0.0.0';
+        
+        if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+            
+          $ip = $_SERVER['HTTP_CLIENT_IP'];
+        
+        } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+          //to check ip is pass from proxy
+          $ip=$_SERVER['HTTP_X_FORWARDED_FOR'];
+          
+        } else {
+            
+          $ip=$_SERVER['REMOTE_ADDR'];
+          
         }
         
         return $ip;
     } 
- }
