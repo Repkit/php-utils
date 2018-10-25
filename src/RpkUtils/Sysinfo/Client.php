6e0be9628ace8505ebeafaefcc366ab1fa1c2a9e
repkit <ionut.baches@yahoo.com>
2018-10-25 09:17:23 +0000

    update get client ip address

--- src/RpkUtils/Sysinfo/Client.php
+++ src/RpkUtils/Sysinfo/Client.php
@@ -22,16 +22,16 @@
     {
         
         if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
-            
+          // check ip from share internet  
           $ip = $_SERVER['HTTP_CLIENT_IP'];
         
         } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
-          //to check ip is pass from proxy
-          $ip=$_SERVER['HTTP_X_FORWARDED_FOR'];
+          // to check ip is pass from proxy
+          $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
           
         } else {
             
-          $ip=$_SERVER['REMOTE_ADDR'];
+          $ip = $_SERVER['REMOTE_ADDR'];
           
         }
         
